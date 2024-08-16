106259d3a637e2b6c2bd8a2840a4f78af20e1418
olalekan <lekanmojibola@gmail.com>
2024-08-16 13:26:54 +0100

    mixed type

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,6 +4,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: int, v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """mixed type"""
     return (k, (v ** 2))
