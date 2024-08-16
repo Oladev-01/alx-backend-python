de2f8333e711ce1076ea402041eb06ddf9132532
olalekan <lekanmojibola@gmail.com>
2024-08-16 16:39:22 +0100

    callable

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""float type annotation"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """return a function"""
+    def mult_float(value: float) -> float:
+        """returning mul of val and multiplier"""
+        return value * multiplier
+    return mult_float
