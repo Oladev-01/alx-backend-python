ea74021db9b5e001fa95ce3fdde104397eb98078
olalekan <lekanmojibola@gmail.com>
2024-08-16 09:38:44 +0100

    typing list sum

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""function annotation of sum"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """return sum of the floats in the list as a float"""
+    return sum(input_list)
