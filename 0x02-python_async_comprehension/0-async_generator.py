ac3a2dde34f207facd4945b52c72e5b246981542
olalekan <lekanmojibola@gmail.com>
2024-08-21 04:34:57 +0100

    yield async

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,8 +4,7 @@ import asyncio
 import random
 from typing import AsyncGenerator
 
-
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[None, float]:
     """yield async"""
     for _ in range(10):
         await asyncio.sleep(1)
