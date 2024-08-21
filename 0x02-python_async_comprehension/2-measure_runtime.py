bb04086f70c21b8c7b4b36e844d579811fc74e6f
olalekan <lekanmojibola@gmail.com>
2024-08-21 04:24:37 +0100

    yield async

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""async comprehension"""
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measure the runtime"""
+    start = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    elapsed = time.perf_counter() - start
+    return elapsed
