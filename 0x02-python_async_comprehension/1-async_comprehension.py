5dfeac44463633d4ef33ffd47c520aa1945d5729
olalekan <lekanmojibola@gmail.com>
2024-08-21 04:27:48 +0100

    yield async

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """yield async"""
-from typing import AsyncGenerator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> AsyncGenerator[float, None]:
+async def async_comprehension() -> List[float]:
     """yeild as comprehension"""
     ran = [ran async for ran in async_generator()]
     return ran
