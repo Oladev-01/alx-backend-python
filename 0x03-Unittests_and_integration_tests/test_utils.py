051eb051ca2d58ade4b9399fca05b60016c4d7d2
olalekan <lekanmojibola@gmail.com>
2024-09-09 18:55:57 +0100

    expect error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,14 @@ class TestAccessNestedMap(unittest.TestCase):
                                expected: Any) -> None:
         """testing nested dict with inputs"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Sequence,
+                                         expected_error: Any) -> None:
+        """test with raise exception"""
+        with self.assertRaises(expected_error):
+            access_nested_map(nested_map, path)
